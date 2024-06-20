8f6314520aa7feeaca24ff66306b21f8d72638f0
Keshav Kumar (MAQ LLC) <v-keshakumar@microsoft.com>
2024-06-20 16:33:59 +0530

    Demo app for fronend of ALM toolkit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+// declare const FEATURE_FLAG: string
+import * as monaco from 'monaco-editor';
+import "../styles.css";
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,4 +12,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'todo-app';
+  // featureFlag = "";
+  constructor() {
+    // this.featureFlag = FEATURE_FLAG;
+    // console.log("Feature Flag: ", this.featureFlag);
+    const container = document.getElementById('container');
+    if (container) {
+      monaco.editor.create(container, {
+        value: 'console.log("Hello, world")',
+        language: 'javascript'
+      });
+    }
+  }
 }
